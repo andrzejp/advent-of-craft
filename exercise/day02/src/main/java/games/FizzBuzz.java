ce52c023746f7d461b1d9049b6518da78e009b94
Andrzej Prochyra <andrzej@prochyra.io>
2023-12-04 17:01:37 +0000

    refactor: Extract range checks

--- exercise/day02/src/main/java/games/FizzBuzz.java
+++ exercise/day02/src/main/java/games/FizzBuzz.java
@@ -5,12 +5,8 @@ public class FizzBuzz {
     }
 
     public static String convert(Integer input) throws OutOfRangeException {
-        if (input <= 0) {
-            throw new OutOfRangeException();
-        }
-        if (input > 100) {
-            throw new OutOfRangeException();
-        }
+        requireGreaterThanZero(input);
+        requireLessThanOneHundred(input);
         if (input % 3 == 0 && input % 5 == 0) {
             return "FizzBuzz";
         }
@@ -21,5 +17,17 @@ public class FizzBuzz {
             return "Buzz";
         }
         return input.toString();
+    }
+
+    private static void requireLessThanOneHundred(Integer input) throws OutOfRangeException {
+        if (input > 100) {
+            throw new OutOfRangeException();
+        }
+    }
+
+    private static void requireGreaterThanZero(Integer input) throws OutOfRangeException {
+        if (input <= 0) {
+            throw new OutOfRangeException();
+        }
     }
 }
