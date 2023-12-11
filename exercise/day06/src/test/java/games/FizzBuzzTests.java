06e511553b44a7eadf8d4bf29f752407ffd8ca3a
Andrzej Prochyra <andrzej@prochyra.io>
2023-12-11 16:13:22 +0000

    refactor: Make test for simple values clearer

--- exercise/day06/src/test/java/games/FizzBuzzTests.java
+++ exercise/day06/src/test/java/games/FizzBuzzTests.java
@@ -1,7 +1,6 @@
 package games;
 
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -10,14 +9,10 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 class FizzBuzzTests {
 
     @ParameterizedTest
-    @CsvSource({
-            "1, 1",
-            "67, 67",
-            "82, 82"
-    })
-    void should_return_number_as_string(int input, String expected) throws OutOfRangeException {
+    @ValueSource(ints = {1, 67, 82})
+    void should_return_number_as_string(int input) throws OutOfRangeException {
         assertThat(FizzBuzz.convert(input))
-                .isEqualTo(expected);
+                .isEqualTo(String.valueOf(input));
     }
 
     @ParameterizedTest
