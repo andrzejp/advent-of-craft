a0c03a566434b560710600e69ce69b2e1c857598
Andrzej Prochyra <andrzej@prochyra.io>
2023-12-11 16:11:27 +0000

    refactor: Use @ValueSource for simple values

--- exercise/day06/src/test/java/games/FizzBuzzTests.java
+++ exercise/day06/src/test/java/games/FizzBuzzTests.java
@@ -2,6 +2,7 @@ package games;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -20,28 +21,28 @@ class FizzBuzzTests {
     }
 
     @ParameterizedTest
-    @CsvSource({"3", "66", "99"})
+    @ValueSource(ints = {3, 66, 99})
     void should_return_Fizz_for_multiples_of_3(int input) throws OutOfRangeException {
         assertThat(FizzBuzz.convert(input))
                 .isEqualTo("Fizz");
     }
 
     @ParameterizedTest
-    @CsvSource({"5", "50", "85"})
+    @ValueSource(ints = {5, 50, 85})
     void should_return_Buzz_for_multiples_of_5(int input) throws OutOfRangeException {
         assertThat(FizzBuzz.convert(input))
                 .isEqualTo("Buzz");
     }
 
     @ParameterizedTest
-    @CsvSource({"15", "30", "45"})
+    @ValueSource(ints = {15, 30, 45})
     void should_return_FizzBuzz_for_multiples_of_both_3_and_5(int input) throws OutOfRangeException {
         assertThat(FizzBuzz.convert(input))
                 .isEqualTo("FizzBuzz");
     }
 
     @ParameterizedTest
-    @CsvSource({"0", "101", "-1"})
+    @ValueSource(ints = {0, 101, -1})
     void should_only_accept_numbers_between_1_and_100(int input) {
         assertThatThrownBy(() -> FizzBuzz.convert(input))
                 .isInstanceOf(OutOfRangeException.class);
